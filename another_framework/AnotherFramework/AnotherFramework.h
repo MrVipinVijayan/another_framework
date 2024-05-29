de30b442f6418135084b9d5e96c5266de3242c69
flutterthemer <flutterthemer@gmail.com>
2024-05-29 12:01:46 -0500

    initial framework push

---
+++ another_framework/AnotherFramework/AnotherFramework.h
@@ -0,0 +1,18 @@
+//
+//  AnotherFramework.h
+//  AnotherFramework
+//
+//  Created by Vipin Vijayan Nair on 5/28/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnotherFramework.
+FOUNDATION_EXPORT double AnotherFrameworkVersionNumber;
+
+//! Project version string for AnotherFramework.
+FOUNDATION_EXPORT const unsigned char AnotherFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnotherFramework/PublicHeader.h>
+
+
---
+++ another_framework/AnotherFramework/HelloWorld.swift
@@ -0,0 +1,19 @@
+//
+//  HelloWorld.swift
+//  AnotherFramework
+//
+//  Created by Vipin Vijayan Nair on 5/29/24.
+//
+
+import Foundation
+
+public class HelloWorld {
+    
+    public init() {
+        
+    }
+    
+    public func printMessage(message: String) {
+        print(message)
+    }
+}
